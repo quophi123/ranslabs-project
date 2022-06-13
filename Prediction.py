d3434f449c2a2fcf559a45d9cef6c6742c9be01a
quophi123 <ransquophi@gmail.com>
2022-06-13 01:26:13 -0700

    made some changes

--- Prediction.py
+++
@@ -1,6 +0,0 @@
-import streamlit as st 
-
-
-
-def app():
-    return st.subheader('predict here')
--- main.py
+++ main.py
@@ -3,28 +3,46 @@ from warnings import catch_warnings
 from pandas.core.indexes.base import Index
 import streamlit as st 
 import pandas as pd
-import Modelbuilding,EDA,Prediction,FileUpload,Home,Visualize
+from PIL import Image
+#import Modelbuilding,EDA,Prediction,FileUpload,Home,Visualize
 from streamlit_option_menu import option_menu
 
-PAGES = {
-    "Home":Home,
-    "File":FileUpload,
-    "Analysis":EDA,
-    "Visuals":Visualize,
-    "Models":Modelbuilding,
-    "Predict":Prediction,
-}
+
+#site configurations 
+logo = Image.open('logo.jpg')
+st.set_page_config(page_title='ml automate', page_icon=logo)
+
+hid_menu_style = """
+            <style>
+            #MainMenu {visibility:hidden;}
+            footer {visibility:hidden;}
+            </style>
+"""
+st.markdown(hid_menu_style,unsafe_allow_html=True)
+
+
+
+
+
+st.header('main')
+#PAGES = {
+ #   "Home":Home,
+ #   "Analysis":EDA,
+ #   "Visuals":Visualize,
+ #   "Models":Modelbuilding,
+ #   "Predict":Prediction,
+#}
 
 
 
 #st.sidebar.title('Activities')
 #selection = st.sidebar.selectbox("Menu", list(PAGES.keys()))
 
-selection = option_menu(
-    menu_title=None,
-    options=list(PAGES.keys()),
-    icons=['house-door-fill','cloud-arrow-up-fill','graph-up-arrow','reception-4','layers-fill','question-octagon-fill'],
-    orientation='horizontal',
+#selection = option_menu(
+    #menu_title=None,
+    #options=list(PAGES.keys()),
+    #icons=['house-door-fill','cloud-arrow-up-fill','graph-up-arrow','reception-4','layers-fill','question-octagon-fill'],
+    #orientation='horizontal',
     #styles={
         #"container": {"padding": "0px","background-color": "red","boader-size":"25px"},
         #"icon": {"color": "orange","font-size": "25px"},
@@ -38,6 +56,6 @@ selection = option_menu(
         #"na-link-selected" : {"background":"green"},
         
 #    }
-)
-page = PAGES[selection]
-page.app()
+#)
+#page = PAGES[selection]
+#page.app()
